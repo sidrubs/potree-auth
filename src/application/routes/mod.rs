06c85a7647c62e84c85b5ef1218f2086c7965b5e
sidrubs <52378223+sidrubs@users.noreply.github.com>
2025-07-18 09:00:27 -0300

    feat(routing): Split routes into more composable structure (#3)

--- src/application/routes/mod.rs
+++ src/application/routes/mod.rs
@@ -11,7 +11,11 @@ use axum::{Extension, Router, routing::get};
 use http::{HeaderValue, header};
 use state::ApplicationState;
 use time::Duration;
-use tower_http::set_header::SetResponseHeaderLayer;
+use tower::Layer;
+use tower_http::{
+    normalize_path::{NormalizePath, NormalizePathLayer},
+    set_header::SetResponseHeaderLayer,
+};
 use web_route::{ParameterizedRoute, WebRoute};
 
 use crate::{
@@ -28,17 +32,19 @@ use super::middleware::{session::apply_session_layer, tracing::apply_tracing_mid
 pub(crate) static HEALTH_CHECK: LazyLock<ParameterizedRoute> =
     LazyLock::new(|| ParameterizedRoute::new("/_health"));
 
-pub(crate) static STATIC_POTREE: LazyLock<ParameterizedRoute> =
-    LazyLock::new(|| ParameterizedRoute::new("/static/potree"));
-pub(crate) static STATIC_POTREE_ASSET_ROUTE: LazyLock<ParameterizedRoute> =
-    LazyLock::new(|| STATIC_POTREE.join("{*asset_path}"));
+pub(crate) static POTREE_ASSETS_ROOT: LazyLock<ParameterizedRoute> =
+    LazyLock::new(|| ParameterizedRoute::new("/potree-assets"));
+pub(crate) static POTREE_ASSETS: LazyLock<ParameterizedRoute> =
+    LazyLock::new(|| POTREE_ASSETS_ROOT.join("/{*path}"));
+pub(crate) static PROJECT_ASSETS_ROOT: LazyLock<ParameterizedRoute> =
+    LazyLock::new(|| ParameterizedRoute::new("/project-assets"));
+pub(crate) static PROJECT_ASSETS: LazyLock<ParameterizedRoute> =
+    LazyLock::new(|| PROJECT_ASSETS_ROOT.join("/{project_id}/{*path}"));
 
-pub(crate) static PROJECT_ROOT: LazyLock<ParameterizedRoute> =
-    LazyLock::new(|| ParameterizedRoute::new("/project"));
-pub(crate) static PROJECT_ROUTE: LazyLock<ParameterizedRoute> =
-    LazyLock::new(|| PROJECT_ROOT.join("/{project_id}"));
-pub(crate) static PROJECT_ASSET: LazyLock<ParameterizedRoute> =
-    LazyLock::new(|| PROJECT_ROUTE.join("/assets/{*path}"));
+pub(crate) static POTREE_UI: LazyLock<ParameterizedRoute> =
+    LazyLock::new(|| ParameterizedRoute::new("/potree"));
+pub(crate) static POTREE_UI_PROJECT: LazyLock<ParameterizedRoute> =
+    LazyLock::new(|| POTREE_UI.join("/{project_id}"));
 
 #[derive(Debug, Clone, serde::Serialize)]
 pub(crate) struct ProjectAssetParams {
@@ -57,7 +63,7 @@ pub fn build_router(
     project_service: Arc<dyn ProjectService>,
     project_asset_service: Arc<dyn ProjectAssetService>,
     potree_asset_service: Arc<dyn PotreeAssetService>,
-) -> Router {
+) -> NormalizePath<Router> {
     // Initialize application state.
     let state = ApplicationState {
         authorization_service,
@@ -76,17 +82,19 @@ pub fn build_router(
     // Build the router.
     let router = Router::new()
         .route(&HEALTH_CHECK, get(health_check::health_check))
-        .route(&STATIC_POTREE_ASSET_ROUTE, get(potree_asset::potree_asset))
+        .route(&POTREE_ASSETS, get(potree_asset::potree_asset))
         .route(
-            &PROJECT_ASSET,
+            &PROJECT_ASSETS,
             get(project_asset::project_asset).layer(add_cache_control),
         )
-        .route(&PROJECT_ROUTE, get(potree_render::potree_render))
+        .route(&POTREE_UI_PROJECT, get(potree_render::potree_render))
         .route(&AUTH_LOGIN, get(auth::login))
         .route(&AUTH_CALLBACK, get(auth::callback))
         .layer(Extension(state));
 
     // Apply middleware
     let router = apply_session_layer(router, Duration::days(1));
-    apply_tracing_middleware(router)
+    let router = apply_tracing_middleware(router);
+
+    NormalizePathLayer::trim_trailing_slash().layer(router)
 }
--- src/application/routes/potree_asset.rs
+++ src/application/routes/potree_asset.rs
@@ -10,13 +10,13 @@ use crate::{
 
 #[derive(Deserialize)]
 pub(crate) struct Params {
-    asset_path: PathBuf,
+    path: PathBuf,
 }
 
 /// Serves a built, static `potree` asset,
 pub(crate) async fn potree_asset(
-    Path(Params { asset_path }): Path<Params>,
+    Path(Params { path }): Path<Params>,
     PotreeAssets(potree_assets): PotreeAssets,
 ) -> Result<StaticAsset, ApplicationError> {
-    potree_assets.get_asset(&asset_path.to_string_lossy()).await
+    potree_assets.get_asset(&path.to_string_lossy()).await
 }
